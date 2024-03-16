5f6ade46245e0e211d613aa470154765c5eccf00
Shypanda0119 <kmkm7936@naver.com>
2024-03-16 22:01:29 +0900

    자바 복습하기 "13일차"

---
+++ chap07/src/sec02/exam02/FieldInitValue.java
@@ -0,0 +1,22 @@
+package sec02.exam02;
+
+public class FieldInitValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte byteField;
+		short shortField;
+		int intField;
+		long longField;
+		
+		boolean booleanField;
+		char charfield;
+		
+		float floatField;
+		double doubleField;
+		
+		int[] arrField;
+		String referenceField;
+	}
+
+}
