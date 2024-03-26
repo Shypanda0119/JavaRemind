b8938812678ec743ff0ce53664d14b6c29059c75
Shypanda0119 <kmkm7936@naver.com>
2024-03-27 01:37:47 +0900

    자바 복습하기 "19일차"

---
+++ chap06/src/quiz02/Quiz02.java
@@ -0,0 +1,28 @@
+package quiz02;
+
+public class Quiz02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] array = {
+				{95, 86},
+				{83, 92, 96},
+				{78, 83, 93, 87, 88}
+		};
+		
+		int sum = 0;
+		int cnt = 0;
+		double avg = 0.0;
+		
+		for(int i=0; i<array.length; i++) {
+			for(int j=0; j<array[i].length;j++) {
+				sum += array[i][j];
+				cnt++;
+				}
+			}
+		avg = (double)sum / cnt;
+		System.out.println("sum: " + sum);
+		System.out.println("avg: " + avg);
+	}
+
+}
