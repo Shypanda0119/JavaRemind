1c1e60cc8f3f806de213f410a915e714e506316e
Shypanda0119 <kmkm7936@naver.com>
2024-03-29 23:44:03 +0900

    자바 복습하기 "23일차"

---
+++ chap07/src/sec06/exam03/package2/C.java
@@ -0,0 +1,12 @@
+package sec06.exam03.package2;
+
+import	sec06.exam03.package1.*; 
+public class C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// A a;
+		B b;
+	}
+
+}
